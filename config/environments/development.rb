8307f8b53c827dc2b385dc2b781a04c9cfb6a8c2
Lakhai <sebasalines@gmail.com>
2015-05-17 23:52:48 -0300

    Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,14 +33,14 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: "storealicious.net",
     authentication: "plain",
-    user_name: "mailski",
-    password: "isecretlylove50cent",
+    user_name: "sebasalines",
+    password: "sebastianv3",
     enable_starttls_auto: true
   }
   # Raises error for missing translations
